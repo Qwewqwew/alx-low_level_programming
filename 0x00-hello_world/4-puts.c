36a41d2d9191eaa60d3dfb5a456a5b34f8fb74d8
root <Qwewqwew>
2023-08-10 15:53:06 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
-int main(){
-	puts("Programming is like building a multilingual puzzle")
-		return(0)
+/**
+ * main - enrty point
+ *
+ * Description : C program using puts funtion
+ *
+ * return : sucsess when returns (0)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+		return (0);
 }
