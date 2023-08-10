95712b4cdaaf2a58eda5cb77c005f3376195d03f
root <Qwewqwew>
2023-08-10 16:15:09 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Description: C program using printf function
+ *
+ * Return:(0) when success
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
