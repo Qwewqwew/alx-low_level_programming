3a1cf6eb1bbfb608a718c5065bc8663a075205a0
root <Qwewqwew>
2023-08-10 17:29:36 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
 */
 int main(void)
 {
-char error = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+char error[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 fwrite(1, error, 59);
 return (1);
 }
