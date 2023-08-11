ddcf823253d5dfd43652e9655f29c10bc2344d99
root <Qwewqwew>
2023-08-11 15:06:09 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,13 +10,16 @@
 int main(void)
 {
 int i = 48;
-while (i < 57)
+while (i < 58)
 {
 putchar(i);
+if (i < 57)
+{
 putchar(',');
 putchar(' ');
+}
 i++;
 }
-putchar(57);
+putchar('\n');
 return (0);
 }
