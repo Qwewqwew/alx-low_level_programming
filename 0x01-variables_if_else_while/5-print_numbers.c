d92f816dfec1caa06bb0c8a32436d18058ab1071
root <Qwewqwew>
2023-08-11 13:46:52 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: c program that prints all
+ * single digits base 10
+ *
+ * Return: (0) success
+*/
+int main(void)
+{
+int i = 0;
+while (i < 10)
+{printf("%d\n", i);
+i++;
+}
+return (0);
+}
