6ab3c8f04854782ca968fc44d5f95b8ebe302bef
root <Qwewqwew>
2023-08-10 17:28:27 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+/**
+ * main - main function
+ *
+ * Description: c program using fwrite function
+ *
+ * Return: (1) fail
+*/
+int main(void)
+{
+char error = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+fwrite(1, error, 59);
+return (1);
+}
