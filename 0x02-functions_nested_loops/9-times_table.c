8ccc2a05e1cf4ec3c0d30c83fa1aa52506c180ce
root <Qwewqwew>
2023-08-16 03:48:40 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,24 +8,24 @@ void times_table(void)
 int i, j, k;
 for (i = 0; i < 10; i++)
 {
-putchar('0');
+_putchar('0');
 for (j = 1; j < 10; j++)
 {
-putchar(',');
+_putchar(',');
 k = i * j;
 if (k < 10)
 {
-putchar(' ');
-putchar(' ');
-putchar(k + '0');
+_putchar(' ');
+_putchar(' ');
+_putchar(k + '0');
 }
 else
 {
-putchar(' ');
-putchar(k / 10 + '0');
-putchar(k % 10 + '0');
+_putchar(' ');
+_putchar(k / 10 + '0');
+_putchar(k % 10 + '0');
 }
 }
-putchar('\n');
+_putchar('\n');
 }
 }
