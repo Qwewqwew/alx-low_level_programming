28908364a6ae88a3cf01cfbcdb509b039b22dda3
root <Qwewqwew>
2023-08-16 15:23:48 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -11,7 +11,7 @@ unsigned long f_1 = 0, f_2 = 1, sum;
 unsigned long f_1_1, f_1_2, f_2_1, f_2_2;
 unsigned long num_1, num_2;
 unsigned long max = 100000000;
-for (c = 1; c < 93; c++)
+for (c = 0; c < 92; c++)
 {
 sum = f_1 + f_2;
 printf("%lu, ", sum);
@@ -22,7 +22,7 @@ f_1_1 = f_1 / max;
 f_2_1 = f_2 / max;
 f_1_2 = f_1 % max;
 f_2_2 = f_2 % max;
-for (c = 93; c < 98; c++)
+for (c = 93; c < 96; c++)
 {
 num_1 = f_1_1 + f_2_1;
 num_2 = f_1_2 + f_2_2;
@@ -32,7 +32,7 @@ num_1 += 1;
 num_2 %= max;
 }
 printf("%lu%lu", num_1, num_2);
-if (c != 97)
+if (c != 95)
 printf(", ");
 f_1_1 = f_2_1;
 f_1_2 = f_2_2;
