0826aee5caf596fcb08dc9f8da66cc9aa93df47a
root <Qwewqwew>
2023-08-16 02:30:45 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,19 @@
+#include <srdio.h>
+#include "main.h"
+/**
+ * jack_bauer - afunction that prints every minuet of the day
+*/
+void jack_bauer(void)
+{
+int h, m;
+for (h = 0; h < 24; h++)
+{
+for (m = 0; m < 60; m++)
+{
+putchar(h / 10 + '0');
+putchar(h % 10 + '0');
+putchar(':');
+putchar(m / 10 + '0');
+putchar(m % 10 + '0');
+}
+}
