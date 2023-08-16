dfe0f1c01cd9b3c1e81654c827f036f4b49ce44a
root <Qwewqwew>
2023-08-16 12:27:50 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,1 +1,23 @@
 #include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: (0) success
+*/
+int main(void)
+{
+int i;
+long n_1 = 0, n_2 = 1, res;
+for (i = 0; i < 50; i++)
+{
+res = n_1 + n_2;
+printf("%ld", res);
+n_1 = n_2;
+n_2 = res;
+}
+if (i == 49)
+printf("\n");
+else
+printf(", ");
+return (0);
+}
