801edf91bebe27b58ac7329c55e23f13a1ddad4e
root <Qwewqwew>
2023-08-17 18:35:07 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,8 +4,7 @@
  * 10 times the nums from 0 to 14
 */
 void more_numbers(void)
-{
-int i, j, k;
+{int i, j, k;
 for (i = 0; i < 10; i++)
 {
 for (j = 0; j <= 14; j++)
@@ -13,7 +12,7 @@ for (j = 0; j <= 14; j++)
 k = j;
 if (j > 9)
 {
-_putchar(j / 10);
+_putchar(j / 10 + '0');
 k = j % 10;
 }
 _putchar(k + '0');
