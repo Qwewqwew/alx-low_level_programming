cf6c4af200c69106e3a02994a116ad63e51cf9a5
root <Qwewqwew>
2023-08-17 17:13:04 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isupper - function that tests wheathe
+ * r the letter is in uppercase
+ *
+ * @c: input to be tested
+ *
+ * Return: (1) for uppercase (0) otherwise
+*/
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+return (1);
+return (0);
+}
