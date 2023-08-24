cf6de20d7dee835808f96b84cb412d00a75038ba
root <Qwewqwew>
2023-08-24 11:09:12 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,16 +6,18 @@
  * @dest: input array
  * @src: input array
  *
- * Return: NULL
+ * Return: void
 */
 char *_strcat(char *dest, char *src)
 {
-int i = 0;
-int j = 0;
+int i;
+int j;
+i = 0;
 while (dest[i] != '\0')
 {
 i++
 }
+j = 0;
 while (src[j] != '\0')
 {
 dest[i] = src[j];
