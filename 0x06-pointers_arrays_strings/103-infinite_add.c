ea39cff191edb742b4dc3a8a93e93e79a1f04509
root <Qwewqwew>
2023-08-24 17:44:50 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * *infinite_add - A function that adds 
+ * *infinite_add - A function that adds
  * two numbers
  * @n1: input number1
  * @n2: input number2
@@ -26,11 +26,11 @@ while (j >= 0 || i >= 0 || l == 1)
 if (i < 0)
 val1 = 0;
 else
-val1 = *(n1 + i) - '0';
+val1 = *(n1 + i)-'0';
 if (j < 0)
 val2 = 0;
 else
-val2 = *(n2 + j) - '0';
+val2 = *(n2 + j)-'0';
 res = val1 + val2 + l;
 if (res >= 10)
 l = 1;
@@ -48,5 +48,4 @@ return (0);
 *(r + k) = '\0';
 rev_string(r);
 return (r);
-
 }
