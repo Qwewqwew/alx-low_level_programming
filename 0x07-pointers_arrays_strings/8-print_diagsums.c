a97e11f209fcf98c3fae9edd8f5577fb0b93019b
Qwewqwew <qwewqwew>
2023-08-29 00:45:08 +0200

    8-print_diagsums.c

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_diagsums - A function that prints the sum of the two
+ * diagonals of a squared matrix
+ * @a: input
+ * @size: input
+*/
+void print_diagsums(int *a, int size)
+{
+int i, j, k;
+for (i = 0; i < size; i++)
+{
+j += a[i];
+k += a[size - i - 1];
+a += size;
+}
+printf("%d, ", j);
+printf("%d\n", k);
+}
