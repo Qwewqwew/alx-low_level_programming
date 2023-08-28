91c16dff32ca114c37da075b5377e16e36af5ab4
Qwewqwew <qwewqwew>
2023-08-28 19:55:20 +0200

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * *_memcpy - A function that copies a memory area
+ * @src: memory area to be copied
+ * @dest: memory area to be copied to
+ * @n: number of bytes to be copied
+ * Return: A pointer to dest
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+while (i < n)
+src[i] = dest[i];
+return (dest);
+}
