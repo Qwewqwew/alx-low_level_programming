a11729d49f42e4f44bef79fab20180b28d0e10e7
Qwewqwew <qwewqwew>
2023-08-29 00:57:39 +0200

    void set_string(char **s, char *to);

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * set_string - A function that sets the value of a
+ * pointer to a character
+ * @s: input
+ * @to: input
+*/
+void set_string(char **s, char *to)
+{
+*s = to;
+}
