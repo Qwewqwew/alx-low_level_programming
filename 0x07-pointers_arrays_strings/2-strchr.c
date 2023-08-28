4e09a2750b16a6e751dcb4487e569780ca9e1663
Qwewqwew <qwewqwew>
2023-08-28 20:18:19 +0200

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * A function that locates a character in a string
+ * @s: string
+ * @c: character to be located
+ * Return: if c is found a pointer to first occurance od c
+ * if c is not found_ NULL
+*/
+char *_strchr(char *s, char c)
+{
+int i;
+for (i = 0; s[i] >= '\0'; i++)
+{
+if (s[i] == c)
+return (s + i);
+}
+return ('\0');
+}
