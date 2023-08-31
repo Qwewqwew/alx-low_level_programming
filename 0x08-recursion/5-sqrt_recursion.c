4872026cd8c8619e6c5f94e1d54ac38a77786bec
root <Qwewqwew>
2023-08-30 19:48:42 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,28 +1,28 @@
 #include "main.h"
-/**
- * _sqrt_recursion - A function that returns a natural
- * square root of a number
- * @n: input integer to find the the root to
- * Return: the natural square root if found or -1
- * if no
-*/
-int act_sqrt(int n, int val)
-int _sqrt_recursion(int n)
-{
-return (act_sqrt(n, 1));
-}
-
+#include <stdio.h>
 /**
  * act_sqrt - calculate the square
  * @n: input to find the sqr for
  * @val: input sqr root
- * return:int
+ * Return:int
 */
-
 int act_sqrt(int n, int val)
 {
 if (val * val == n)
 return (val);
 else if (val * val < n)
 return (act_sqrt(n, (val - 1)));
+else
+return (-1);
+}
+/**
+ * _sqrt_recursion - A function that returns a natural
+ * square root of a number
+ * @n: input integer to find the the root to
+ * Return: the natural square root if found or -1
+ * if not
+*/
+int _sqrt_recursion(int n)
+{
+return (act_sqrt(n, 1));
 }
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,12 @@
+
+#include "main.h"
+/**
+ * is_prime_number - A function that returns 1 if the
+ * input is a prime number and 0 if otherwise
+ * @n: input integer
+ * Return: 1 if prime number an 0 if not
+*/
+int is_prime_number(int n)
+{
+
+}
