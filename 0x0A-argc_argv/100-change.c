99e5907c5f9094f221b1ea3ee03eb11bfc92ed66
root <Qwewqwew>
2023-09-01 11:51:39 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -9,10 +9,10 @@
 */
 int main(int argc, char *argv[])
 {
-int i, change = 0, money = atoi(argv[1]);
-int cents[] = {25, 10, 5, 2, 1};
 if (argc == 2)
 {
+int i, change = 0, money = atoi(argv[1]);
+int cents[] = {25, 10, 5, 2, 1};
 for (i = 0; i < 5; i++)
 {
 if (money >= cents[i])
