c2e36f46882a31e71a1ea9ff78fc3c2567d86fff
qwewqwew <Qwewqwew>
2023-09-15 14:11:53 -0700

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -12,6 +12,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 va_list strs;
 char *str;
 unsigned int i;
+va_start(strs, n);
 for (i = 0; i < n; i++)
 {
 str = va_arg(strs, char *);
