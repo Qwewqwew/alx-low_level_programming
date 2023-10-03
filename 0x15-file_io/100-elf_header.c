b666d144212dac3c917c174f2e0df567b80e8572
Qwewqwew <Qwewqwew>
2023-10-03 13:54:14 -0700

    100-elf_header

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -242,3 +242,50 @@ dprintf(STDERR_FILENO,
 exit(98);
 }
 }
+
+/**
+ * main - entry point that displays informs in the ELF file
+ * at start
+ * @argc: number of arguments
+ * @argv: an array of the arguments
+ * Return: 0 on success
+*/
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+Elf64_Ehdr *header;
+int op, re;
+op = open(argv[1], O_RDONLY);
+if (op == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
+exit(98);
+}
+header = malloc(sizeof(Elf64_Ehdr));
+if (header == NULL)
+{
+close_elf(op);
+dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
+exit(98);
+}
+re = read(op, header, sizeof(Elf64_Ehdr));
+if (re == -1)
+{
+free(header);
+close_elf(op);
+dprintf(STDERR_FILENO, "Error: `%s`: No such file\n", argv[1]);
+exit(98);
+}
+check_elf(header->e_ident);
+printf("ELF Header:\n");
+print_magic(header->e_ident);
+print_class(header->e_ident);
+print_data(header->e_ident);
+print_version(header->e_ident);
+print_osabi(header->e_ident);
+print_abi(header->e_ident);
+print_type(header->e_type, header->e_ident);
+print_entry(header->e_entry, header->e_ident);
+free(header);
+close_elf(op);
+return (0);
+}
