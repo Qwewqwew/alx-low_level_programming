948809f03b79902b3f38e23252b155fe67e0428b
Qwewqwew <hajar.mahasi2@gmail.com>
2023-10-27 12:45:15 -0700

    static library libmy.a

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * *_strchr - A function that locates a character in a string
+ * @s: string
+ * @c: character to be located
+ * Return: if c is found - a pointer to first occurance od c
+ * if c is not found - NULL
+*/
+char *_strchr(char *s, char c)
+{
+int i;
+for (i = 0; s[i] >= '\0'; i++)
+{
+if (s[i] == c)
+{
+return (s + i);
+}
+}
+return ('\0');
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _abs - print the abs value
+ *
+ * @i: input to return abs
+ *
+ * Return: the abs value
+*/
+int _abs(int i)
+{
+if (i < 0)
+return (-i);
+return (i);
+}
