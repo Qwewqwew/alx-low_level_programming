a81e58041483e1ce77ed2b7610678f51407746b1
Qwewqwew <hajar.mahasi2@gmail.com>
2023-11-06 12:48:26 -0800

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - A fumction that prints the name of the current
+ * source file
+ *
+ * Return: 0 on success
+*/
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
