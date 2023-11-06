0864107da41b4b2bc48f928ce06add8b83c951c7
Qwewqwew <hajar.mahasi2@gmail.com>
2023-11-06 13:14:48 -0800

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _puts - A function that prints a string followed by a new line
+ * @str: the string to be printed
+*/
+void _puts(char *str)
+{
+int i;
+for (i = 0; str[i] != '\0'; i++)
+_putchar(str[i]);
+_putchar(\n);
+}
