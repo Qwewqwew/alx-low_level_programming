29225f18afc4aa737e432b9d394efa93cff5475a
Qwewqwew <hajar.mahasi2@gmail.com>
2024-01-12 11:53:34 -0800

    libmy.a

---
+++ 0x09-static_libraries/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * *_memcpy - A function that copies a memory area
+ * @src: memory area to be copied
+ * @dest: memory area to be copied to
+ * @n: number of bytes to be copied
+ * Return: A pointer to dest
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i = 0;
+while (i < n)
+{
+dest[i] = src[i];
+i++;
+}
+return (dest);
+}
