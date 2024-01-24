af6a0a8346b01d4ff16236b85d7789e0310e875b
Qwewqwew <hajar.mahasi2@gmail.com>
2024-01-24 08:53:32 -0800

    3-calc.h

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,57 @@
+#include "3-calc.h"
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+/**
+ * op_add - Returns the sum of a & b
+ * @a: first num
+ * @b: second num
+ * Return: sum of a & b
+*/
+int op_add(int a, int b)
+{
+return (a + b);
+}
+/**
+ * op_sub - Returns difference between a & b
+ * @a: first num
+ * @b: second num
+ * Return: difference between a & b
+*/
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+/**
+ * op_mul - Returns multiplication of a & b
+ * @a: first num
+ * @b: second num
+ * Return: a multiplied by b
+*/
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+/**
+ * op_div - Returns a devided by b
+ * @a: first num
+ * @b: second num
+ * Return: a devided by b
+*/
+int op_div(int a, int b)
+{
+return (a / b);
+}
+/**
+ * op_mod - Returns the modlus when a is devided by b
+ * @a: first num
+ * @b: second num
+ * Return: modlus
+*/
+int op_mod(int a, int b)
+{
+return (a % b);
+}
